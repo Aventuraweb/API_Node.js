0b9b3d7679c22a7e5f8578e850f8a0703f7e7d4d
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-08-12 20:50:48 -0500

    Ccreacion de conexion para crud de forma sencilla

---
+++ server/db/connection/db.js
@@ -0,0 +1,20 @@
+
+// forma sencilla de hacer la conexion del crud 
+
+// import mysql, { createConnection } from "mysql2"
+
+// const connectInfo = {
+//     host : "localhost", 
+//     user: "root", 
+//     password: "your_password", 
+//     database: "your_database"
+// }; 
+
+// const connection = mysql.createConnection(connectInfo)
+
+// connection.connect((err) =>{
+//     if(err) throw err; 
+//     console.log("connected to MySql database"); 
+// })
+
+// export default connection; 
